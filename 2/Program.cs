8a5800fa63f738454407ffdd6d3d805141bda7da
Olga <miarik@yandex.ru>
2022-09-18 10:23:12 -0400

    3d commit

--- 2/Program.cs
+++ 2/Program.cs
@@ -3,11 +3,18 @@
     static char[] SwapNumbers(int num)
     {
         char[]arr = num.ToString().ToCharArray();
-        char temp = arr[arr.Length-1];
-        arr[arr.Length-1] = arr[arr.Length-2];
-        arr[arr.Length-2] = arr[0];
-        arr[0] = temp;
-        return arr;
+        // char temp = arr[arr.Length-1];
+        // arr[arr.Length-1] = arr[arr.Length-2];
+        // arr[arr.Length-2] = arr[0];
+        // arr[0] = temp;
+        // return arr;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            char temp = arr[arr.Length-1];
+            arr[i] = arr[i+1];
+            arr[0] = temp;
+            return arr;
+        }
     }
 
     static void PrintArray(char[] arr)
