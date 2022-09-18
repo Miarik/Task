b4a6d2d5608b147d8b27931cf1fd1b2e9fd01ff4
Olga <miarik@yandex.ru>
2022-09-18 06:27:49 -0400

    2 commit

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,7 +1,17 @@
-﻿ int[] SwapNumbers(int num)
-    {}
-        char[]arr = num.ToString().ToCharArray();
-        int temp = arr[arr.Length-1];
-        arr[arr.Length-1] = arr[arr.Length-2];
-        arr[0] = temp;
+﻿
+class HelloWorld
+{
+    static double CountX(int x)
+    {
+        int x2 = x*x;
+        double res = (3 * Math.Pow(x,3) + 18 * Math.Pow(x,2))*x +12*Math.Pow(x,2) - 5;
+        return res;
     }
+        
+
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+       Console.WriteLine(CountX(x));
+    }
+}
